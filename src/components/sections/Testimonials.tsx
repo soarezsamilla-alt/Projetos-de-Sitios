a57a407c4c8ed1e065d9e13195a5a70791e0979f
Samilla Soarez <soarezsamilla@gmail.com>
2026-05-02 00:21:23 +0000

    pode aumente mais um pouco

--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -53,7 +53,7 @@ export function Testimonials() {
               {testimonials.map((t, idx) => {
                 const imgData = PlaceHolderImages.find(img => img.id === t.id);
                 return (
-                  <CarouselItem key={idx} className="pl-4 basis-[80%] sm:basis-1/2 md:basis-[40%] lg:basis-[30%]">
+                  <CarouselItem key={idx} className="pl-4 basis-[90%] sm:basis-[60%] md:basis-[50%] lg:basis-[35%]">
                     <div className="relative aspect-[9/16] bg-card border border-border rounded-sm overflow-hidden shadow-2xl group flex items-center justify-center p-2">
                        <div className="absolute inset-0 bg-primary/5 group-hover:bg-transparent transition-colors z-10 pointer-events-none" />
                        {imgData ? (
